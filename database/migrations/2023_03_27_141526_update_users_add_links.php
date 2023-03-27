3bf4ab7a3dc3dd2a96b0eb3e363abe5d8e05a1bb
Mateus Silva <mateus_silva97@hotmail.com>
2023-03-27 14:28:47 -0300

    fix(user): fixed reverse migrate

--- database/migrations/2023_03_27_141526_update_users_add_links.php
+++ database/migrations/2023_03_27_141526_update_users_add_links.php
@@ -22,6 +22,9 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('linkedin_url');
+            $table->dropColumn('github_url');
+        });
     }
 };
